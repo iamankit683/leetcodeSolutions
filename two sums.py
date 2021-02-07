3944fdd67e8bdb2e18ea2e8e4e8a64a605fdbf04
iamankit683 <iamankit583@gmail.com>
2021-02-07 23:29:21 +0530

    Leetcode Solutions

--- two sums.py
+++ two sums.py
@@ -4,4 +4,4 @@ class Solution
         for i in range (length-1)
             for j in range(i+1,length)
                 if nums[i]+nums[j]==target
-                    return {i,j]
+                    return {i,j}
